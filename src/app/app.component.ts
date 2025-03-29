163feea14615f3a51f03279e5fd65082ff8cf17f
Kavshree <bjkavyashree@gmail.com>
2025-03-30 03:39:02 +0530

    added shared service singleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserServiceLibraryService } from 'kavya-myuser-service-library';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [UserServiceLibraryService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'hotel-reservation-system';
+  constructor(private userService: UserServiceLibraryService) {
+
+  }
+
+  ngOnInit() {
+    this.userService.loggedIn$.subscribe(res => {
+      console.log("from shell",this.userService.isLoggedIn, res)
+    })
+  }
+
+  login() {
+    this.userService.login()
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-
   name: 'hotel-reservation-system',
   filename: 'remoteEntry.js',
   remotes: {
     './Component': './src/app/app.component.ts',
   },
-
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll(
+      { singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+      'kavya-myuser-service-library': {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: '^0.0.1'
+    }
   },
-
 });
